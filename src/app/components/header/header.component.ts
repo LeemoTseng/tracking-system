71702dc12a73145ed00c16b3a07a49a702ee5ec5
Leemo Tseng <LeemoTseng@tpe.thi-group.com>
2024-12-24 15:29:39 +0800

    update menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule,RouterLink],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './header.component.html',
 
 })
-export class HeaderComponent {
-  
+export class HeaderComponent implements OnInit {
+
+  // router = Router
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.detectUrl(this.router.url);
+    this.router.events.subscribe(() => {
+      this.detectUrl(this.router.url); 
+    });
+  }
+
+  menuList: any[] = [
+    {
+      name: 'Shipment Summary',
+      routerLink: '/shipment-summary'
+    }, {
+      name: 'Shipment List',
+      routerLink: '/shipment-list'
+    },
+  ]
+
+  selectedMenu: string = ""
+  selectMenu(item: string) {
+    this.selectedMenu = item
+  }
+
+  detectUrl(currentUrl:string){
+    const matchedMenu = this.menuList.find(menu => menu.routerLink === currentUrl);
+    this.selectedMenu = matchedMenu ? matchedMenu.name : '';
+  }
+
 }
