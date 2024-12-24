cf50eb0627d0c42ccefef8937cd2a2928a86c6e0
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2024-12-24 18:02:27 +0800

    update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -11,14 +12,17 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  // router = Router
+  // Router
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.detectUrl(this.router.url);
-    this.router.events.subscribe(() => {
-      this.detectUrl(this.router.url); 
-    });
+    this.findUrl(this.router.url);
+    // console.log('this.router.url', this.router.url);
+      this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.findUrl(event.urlAfterRedirects);
+      });
   }
 
   menuList: any[] = [
@@ -36,8 +40,9 @@ export class HeaderComponent implements OnInit {
     this.selectedMenu = item
   }
 
-  detectUrl(currentUrl:string){
+  findUrl(currentUrl:string){
     const matchedMenu = this.menuList.find(menu => menu.routerLink === currentUrl);
+    console.log('matchedMenu', matchedMenu);
     this.selectedMenu = matchedMenu ? matchedMenu.name : '';
   }
 
