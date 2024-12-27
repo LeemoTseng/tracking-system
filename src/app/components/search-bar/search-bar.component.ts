e3a897066c720b8ed0f83e615bd18112b68a24a4
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2024-12-27 17:53:34 +0800

    update search.component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,17 +1,33 @@
-import { Component, HostListener } from '@angular/core';
-import { MatRippleModule } from '@angular/material/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [MatIconModule, MatRippleModule, MatMenuModule],
+  imports: [MatIconModule, MatRippleModule, MatMenuModule, MatDatepickerModule,
+    MatFormFieldModule, MatNativeDateModule, FormsModule, MatButtonModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
   rippleColor = 'rgba(0,0,0,0.05)';
+
+  value = 'Clear me';
+
+  onFocus(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    inputElement.showPicker?.(); 
+    inputElement.focus(); 
+  }
+  
+  startDate = '';
+  endDate = '';
 
   // if clicked outside the search box, unfocus
   isFocused = false;
@@ -20,6 +36,7 @@ export class SearchBarComponent {
     this.isFocused = true;
     console.log(this.isFocused);
   }
+
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent): void {
     if (!this.isFocused) {
@@ -31,4 +48,7 @@ export class SearchBarComponent {
       console.log(this.isFocused);
     }
   }
+
+
+
 }
