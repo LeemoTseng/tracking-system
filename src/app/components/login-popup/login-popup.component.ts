6d179261bc739a15e223c24c70bee7119173f80a
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2024-12-30 18:03:25 +0800

    update popup layout

--- src/app/components/login-popup/login-popup.component.ts
+++ src/app/components/login-popup/login-popup.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login-popup',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatRippleModule],
   templateUrl: './login-popup.component.html',
 })
 export class LoginPopupComponent {
+
+  rippleColor= 'rgba(255, 255, 255, 0.1)';
+
+  closePopup(){
+    console.log('Closing popup');
+    
+  }
+
 
 }
